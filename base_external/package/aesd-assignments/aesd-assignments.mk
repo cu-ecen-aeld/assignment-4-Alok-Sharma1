18093cd088b3d63bf7f7cc2f857fe6581524f51b
Alok-Sharma1 <alok.sharma1002@gmail.com>
2026-05-07 00:03:08 +0530

    Assignment 5 complete: Buildroot port forwarding and init script integration

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = b2b56f02241acbe6c8fc79259ecd4719bbaddbe2
+AESD_ASSIGNMENTS_VERSION = b8fff8d9c8e44ebf8e0f7bb0fe6527f32190e58c
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -16,10 +16,13 @@ AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
 endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
 	# Create the conf directory
 	$(INSTALL) -d 0755 $(TARGET_DIR)/etc/finder-app/conf/
 	
